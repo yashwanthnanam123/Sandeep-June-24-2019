807eb92de1c5498b00fa8533ed7b9fe70f38bfa5
yashwanthnanam123 <35082663+yashwanthnanam123@users.noreply.github.com>
2019-06-24 16:18:26 -0400

    Adding terraform config files

---
+++ main.tf
@@ -0,0 +1,118 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+# Define our VPC
+resource "aws_vpc" "default" {
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "test-vpc"
+  }
+}
+# Define the public subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.public_subnet_cidr}"
+  availability_zone = "us-east-1a"
+
+  tags {
+    Name = "Public Subnet"
+  }
+}
+
+# Define the private subnet
+resource "aws_subnet" "private-subnet" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.private_subnet_cidr}"
+  availability_zone = "us-east-1b"
+
+  tags {
+    Name = "Private Subnet"
+  }
+}
+# Define the internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "VPC IGW"
+  }
+}
+
+# Define the route table
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "Public Subnet RT"
+  }
+}
+
+# Assign the route table to the public Subnet
+resource "aws_route_table_association" "web-public-rt" {
+  subnet_id = "${aws_subnet.public-subnet.id}"
+  route_table_id = "${aws_route_table.web-public-rt.id}"
+}
+
+# Define the security group for public subnet
+resource "aws_security_group" "sgweb" {
+  name = "vpc_test_web"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  vpc_id="${aws_vpc.default.id}"
+
+  tags {
+    Name = "Web Server SG"
+  }
+}
+
+# Define webserver inside the public subnet
+resource "aws_instance" "wb" {
+   ami  = "${var.ami}"
+   instance_type = "t1.micro"
+   key_name = "${aws_key_pair.default.id}"
+   subnet_id = "${aws_subnet.public-subnet.id}"
+   vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
+   associate_public_ip_address = true
+   source_dest_check = false
+   user_data = "${file("install.sh")}"
+
+  tags {
+    Name = "webserver"
+  }
+}
